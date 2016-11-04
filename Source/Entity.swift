aa93e6e7f895949cfbec4d8f5f80130eb40403c5
Konstantin Sukharev <poed@me.com>
2016-11-04 17:31:29 +0300

    Add entity relations

--- Source/Entity.swift
+++ Source/Entity.swift
@@ -16,8 +16,10 @@ extension Entity: Hashable {
 public final class EntityManager {
 	typealias RemoveHandle = () -> ()
 
-	private var unusedID: UInt64 = 0
-	private var removeHandles: [Entity: [StoreID: RemoveHandle]] = [:]
+	private var unusedID = 0 as UInt64
+	private var removeHandles = [:] as [Entity: [StoreID: RemoveHandle]]
+	private var parents = [:] as [Entity: Entity]
+	private var children = [:] as [Entity: Set<Entity>]
 
 	public func create() -> Entity {
 		let entity = Entity(id: unusedID)
@@ -25,17 +27,37 @@ public final class EntityManager {
 		return entity
 	}
 
+	public func create(boundTo parent: Entity) -> Entity {
+		let entity = create()
+		parents[entity] = parent
+		var parentChildren = children[parent] ?? []
+		parentChildren.insert(entity)
+		children[parent] = parentChildren
+		return entity
+	}
+
 	public func removeEntity(_ entity: Entity) {
-		if let handles = removeHandles.removeValue(forKey: entity) {
-			for handle in handles.values {
-				handle()
+		if let children = children.removeValue(forKey: entity) {
+			for child in children {
+				removeEntity(child)
 			}
 		}
+
+		parents[entity] = nil
+		removeComponents(for: entity)
 	}
 
 	func setRemoveHandle(entity: Entity, storeID: StoreID, handle: RemoveHandle?) {
 		var handles = removeHandles[entity] ?? [:]
 		handles[storeID] = handle
 		removeHandles[entity] = handles
+	}
+
+	private func removeComponents(for entity: Entity) {
+		if let handles = removeHandles.removeValue(forKey: entity) {
+			for handle in handles.values {
+				handle()
+			}
+		}
 	}
 }
